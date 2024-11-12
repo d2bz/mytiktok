2fa0d614ad86acb47f1becbb6c4a827df69d9a8a
tiktok-server <3180324210@qq.com>
2024-11-12 21:40:45 +0800

    feat: user follow service

--- internal/repository/redisDB/key.go
+++ internal/repository/redisDB/key.go
@@ -4,4 +4,6 @@ const (
 	LOGIN_USER_EMAIL = "user:login:"
 
 	VIDEO_LIKED = "video:liked:"
+
+	USER_FOLLOW = "user:follow:"
 )
--- internal/service/userService/follow.go
+++ internal/service/userService/follow.go
@@ -1,40 +1,59 @@
 package userService
 
 import (
-	//"context"
+	"context"
 	"tiktok/internal/repository/mysqlDB"
-	//"tiktok/internal/repository/redisDB"
+	"tiktok/internal/repository/redisDB"
 )
 
-func IsFollow(uid string, toFollowUid string) {
+func IsFollow(uid string, toFollowUid string) (bool, error) {
+	db := mysqlDB.GetDB()
+	var cnt int64
+	if err := db.Model(&mysqlDB.Follow{}).
+		Where("user_id = ? AND followed_user_id = ?", uid, toFollowUid).
+		Count(&cnt).Error; err != nil || cnt == 0 {
+		return false, err
+	}
 
+	return true, nil
 }
 
-func Follow(uid string, toFollowUid string) {
-	IsFollow(uid, toFollowUid)
+func Follow(uid string, toFollowUid string) (int, error) {
+	isFollow, err := IsFollow(uid, toFollowUid)
+	if err != nil {
+		return 0, err
+	}
 
 	db := mysqlDB.GetDB()
-	// cbg := context.Background()
-	// rdb := redisDB.GetRDB()
-	// key
+	cbg := context.Background()
+	rdb := redisDB.GetRDB()
+	key := redisDB.USER_FOLLOW + uid
 
 	followMsg := &mysqlDB.Follow{
 		UserID:         uid,
 		FollowedUserID: toFollowUid,
 	}
 
-	if true {
+	if isFollow {
 		if err := db.Create(followMsg).Error; err != nil {
-			return
+			return 0, err
 		}
 
-		// msg存入redis
+		if _, err := rdb.SAdd(cbg, key, toFollowUid).Result(); err != nil {
+			return 0, err
+		}
+
+		return 1, nil
 	} else {
-		//改成硬删除
-		if err := db.Delete(followMsg).Error; err != nil {
-			return
+		//硬删除
+		if err := db.Unscoped().Delete(followMsg).Error; err != nil {
+			return 0, err
 		}
 
-		// 从set删除取关的博主
+		if _, err := rdb.SRem(cbg, key, toFollowUid).Result(); err != nil {
+			return 0, err
+		}
+
+		return -1, nil
 	}
 }
